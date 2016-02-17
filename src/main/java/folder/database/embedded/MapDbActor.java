f002111395b6933dccc5f2e8f05a3369f7178ed0
dmitry <dimaaasik.s@gmail.com>
2016-02-17 22:21:32 +0600

    Added mapdb actor and handlers for storaging first data for uploading on remote server.

---
+++ src/main/java/folder/database/embedded/MapDbActor.java
@@ -0,0 +1,83 @@
+package folder.database.embedded;
+
+import info.smart_tools.smartactors.core.*;
+import info.smart_tools.smartactors.core.actors.Actor;
+import info.smart_tools.smartactors.core.actors.annotations.Handler;
+import info.smart_tools.smartactors.core.impl.SMObject;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * Actor for work with mapdb
+ */
+public class MapDbActor extends Actor {
+
+    private Field<String> collectionNameF;
+    private Field<IObject> insertDataF;
+    private Field<IObject> searchDataF;
+    private ListField<String> searchKeysF;
+
+    private DB db;
+
+    public MapDbActor(IObject params) {
+
+        String dbFilePath, dbFilePassword;
+        try {
+            dbFilePath = new Field<String>(new FieldName("dbFilePath")).from(params, String.class);
+            dbFilePassword = new Field<String>(new FieldName("password")).from(params, String.class);
+            collectionNameF = new Field<>(new FieldName("collectionName"));
+            insertDataF = new Field<>(new FieldName("insertData"));
+            searchDataF = new Field<>(new FieldName("searchData"));
+            searchKeysF = new ListField<>(new FieldName("searchKeys"));
+        } catch (ReadValueException | ChangeValueException e) {
+            String err = "An error occurred while creating actor: " + e;
+            System.out.println(err);
+            throw new RuntimeException(err);
+        }
+
+        db = DBMaker.fileDB(new File(dbFilePath))
+                .closeOnJvmShutdown()
+                .encryptionEnable(dbFilePassword)
+                .make();
+    }
+
+    @Handler("insert")
+    public void insert(IMessage msg) throws ReadValueException, ChangeValueException {
+
+        try {
+            Map<String, String> map = db.treeMap(collectionNameF.from(msg, String.class));
+            IObject insertData = insertDataF.from(msg, IObject.class);
+            IObjectIterator it = insertData.iterator();
+            while (it.next()) {
+                map.put(it.getName().toString(), it.getValue().toString());
+            }
+            db.commit();
+        } catch (Exception e) {
+
+            System.out.println("An error occurred while inserting data in mapdb: " + e);
+            db.rollback();
+        }
+    }
+
+    @Handler("get")
+    public void get(IMessage msg) throws ReadValueException, ChangeValueException {
+
+        try {
+            Map<String, String> map = db.treeMap(collectionNameF.from(msg, String.class));
+            IObject searchData = new SMObject();
+            for (String key : searchKeysF.from(msg, String.class)) {
+                String data = map.get(key);
+                if (data != null) {
+                    searchData.setValue(new FieldName(key), data);
+                }
+            }
+
+            searchDataF.inject(msg, searchData);
+        } catch (Exception e) {
+            System.out.println("An error occurred while inserting data in mapdb: " + e);
+        }
+    }
+}
--- src/main/java/folder/upload/UploadActor.java
+++ src/main/java/folder/upload/UploadActor.java
@@ -3,7 +3,9 @@ package folder.upload;
 import info.smart_tools.smartactors.core.*;
 import info.smart_tools.smartactors.core.actors.Actor;
 import info.smart_tools.smartactors.core.actors.annotations.Handler;
+import info.smart_tools.smartactors.core.impl.SMObject;
 
+import java.io.File;
 import java.util.UUID;
 
 /**
@@ -11,27 +13,129 @@ import java.util.UUID;
  */
 public class UploadActor extends Actor {
 
-    private Field<String> relativePathF;
+    /** Относительный путь внутри виртуальной директории, куда перемещается файл */
+    private Field<String> folderDestPathF;
+    /** Расположение обрабатываемого файла на компьютере */
+    private Field<String> filePathF;
+    /** Тут  путь к системной директории приложения в которой на клиенте хранятся все файлы */
     private Field<String> storageFolderF;
-    private Field<String> filenameF;
+    /** Полный путь к файлу который хранится в системной директории приложения */
+    private Field<String> filePathAbsF;
+    /** Настоящее имя файла*/
+    private Field<String> fileOriginNameF;
+    /** Имя, присвоенное файлу, для хранения в системной директории приложения */
+    private Field<String> fileIdF;
+    private Field<Long> fileSizeF;
     private Field<String> srcF;
     private Field<String> destF;
+    /** Данные для post-запроса*/
+    private Field<IObject> postRequestDataF;
+    /** Поле для имени коллекции для сохранения */
+    private Field<String> collectionNameF;
+    /** Поле с данными для сохранение*/
+    private Field<IObject> insertDataF;
 
+    /** Информация по файлу */
+    private Field<Integer> sentPartsF;
+    private Field<Integer> partsQuantityF;
+    private Field<Boolean> isSentF;
+
+    private String fileInfoCollectionName;
 
     public UploadActor(IObject params) {
 
-        relativePathF = new Field<>(new FieldName("relativePath"));
+        try {
+            fileInfoCollectionName = new Field<String>(new FieldName("fileInfoCollectionName")).from(params, String.class);
+        } catch (ReadValueException | ChangeValueException e) {
+            System.out.println("An error occurred while constructing UploadActor: " + e);
+        }
+        filePathF = new Field<>(new FieldName("filePath"));
         storageFolderF = new Field<>(new FieldName("storageFolder"));
-        filenameF = new Field<>(new FieldName("filename"));
+        filePathAbsF = new Field<>(new FieldName("filePathAbs"));
+        fileSizeF = new Field<>(new FieldName("fileSize"));
         srcF = new Field<>(new FieldName("src"));
         destF = new Field<>(new FieldName("dest"));
+        postRequestDataF = new Field<>(new FieldName("postRequestData"));
+        folderDestPathF = new Field<>(new FieldName("folderDestPath"));
+        fileOriginNameF = new Field<>(new FieldName("fileOriginName"));
+        fileIdF = new Field<>(new FieldName("fileId"));
+        sentPartsF = new Field<>(new FieldName("sentParts"));
+        partsQuantityF = new Field<>(new FieldName("partsQuantity"));
+        isSentF = new Field<>(new FieldName("isSent"));
     }
 
-    @Handler("prepareForLocalStorage")
-    public void prepareForLocalStorage(IMessage msg) throws ReadValueException, ChangeValueException {
+    /**
+     * Записать абсолютный путь источника и целевого файла для хранения в системной директории
+     * @param msg содержит filePath - местоположение файла на локальной машине
+     *                     filePathAbs - местоположение копии файла для работы приложения
+     * @throws ReadValueException
+     * @throws ChangeValueException
+     */
+    @Handler("prepareInfoForCopy")
+    public void prepareInfoForCopy(IMessage msg) throws ReadValueException, ChangeValueException {
 
-        String filename = storageFolderF.from(msg, String.class) + UUID.randomUUID();
-        srcF.inject(msg, relativePathF.from(msg, String.class));
-        destF.inject(msg, filename);
+        String filePathAbs = filePathAbsF.from(msg, String.class);
+        srcF.inject(msg, filePathF.from(msg, String.class));
+        destF.inject(msg, filePathAbs);
+    }
+
+    /**
+     * Генерирует  необходимые для локального и удаленного храрения данные и записывает их в сообщение
+     * @param msg содержит filePath - местоположение файла на локальной машине
+     *                     filePathAbs - местоположение копии файла для работы приложения
+     *                     storageFolderF - директория для хранения файлов внутри системной директории
+     * @throws ReadValueException
+     * @throws ChangeValueException
+     */
+    @Handler("prepareFileMetadata")
+    public void prepareFileMetadata(IMessage msg) throws ReadValueException, ChangeValueException {
+
+        String[] filePathTokens = filePathF.from(msg, String.class).split("\\\\");
+        String fileOriginName = filePathTokens[filePathTokens.length - 1];
+        fileOriginNameF.inject(msg, fileOriginName);
+        String filePathAbs = storageFolderF.from(msg, String.class) + UUID.randomUUID().toString();
+        filePathAbsF.inject(msg, filePathAbs);
+        File file = new File(filePathAbs);
+        fileSizeF.inject(msg, file.length());
+    }
+
+    /**
+     * Готовит начальные данные о файле для контроля загрузки на удаленный сервер
+     * @param msg
+     * @throws ReadValueException
+     * @throws ChangeValueException
+     */
+    @Handler("prepareFileInfoForStorage")
+    public void prepareFileInfoForStorage(IMessage msg) throws ReadValueException, ChangeValueException {
+
+        IObject insertObj = new SMObject();
+        IObject fileInfo = new SMObject();
+        sentPartsF.inject(fileInfo, 0);
+        isSentF.inject(fileInfo, Boolean.FALSE);
+        fileSizeF.inject(fileInfo, fileSizeF.from(msg, Long.class));
+        filePathF.inject(fileInfo, filePathAbsF.from(msg, String.class));
+        insertObj.setValue(new FieldName(filePathF.from(msg, String.class)), fileInfo);
+
+        collectionNameF.inject(msg, fileInfoCollectionName);
+        insertDataF.inject(msg, insertObj);
+    }
+
+    /**
+     * Упаковать информацию о файле для отправки на удаленный сервер
+     * @param msg содержит filePath - местоположение файла на локальной машине
+     *                     fileSize - размер файла
+     *                     fileOriginName - оригинальное имя файла
+     * @throws ReadValueException
+     * @throws ChangeValueException
+     */
+    @Handler("formInfoForServer")
+    public void formInfoForServer(IMessage msg) throws ReadValueException, ChangeValueException {
+
+        //IOC.resolve(IObject.class);??
+        IObject info = new SMObject();
+        fileSizeF.inject(info, fileSizeF.from(msg, Long.class));
+        fileOriginNameF.inject(info, fileOriginNameF.from(msg, String.class));
+        fileIdF.inject(info, filePathF.from(msg, String.class));
+        postRequestDataF.inject(msg, info);
     }
 }
